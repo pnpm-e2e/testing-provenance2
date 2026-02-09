ce9faaf20d8066194d32e3d13a546ec90daeff11
khai96_ <hvksmr1996@gmail.com>
2026-02-09 22:05:20 +0700

    chore: script to bump version and tag

---
+++ bump-version-and-tag.js
@@ -0,0 +1,14 @@
+#! /usr/bin/env node
+const fs = require('fs')
+const { spawnSync } = require('exec-inline')
+const manifest = require('./package.json')
+const [major, minor, oldPatch] = manifest.version.split('.')
+const newPatch = Number(oldPatch) + 1
+manifest.version = `${major}.${minor}.${newPatch}`
+fs.writeFileSync(
+  require.resolve('./package.json'),
+  JSON.stringify(manifest, undefined, 2) + '\n',
+)
+spawnSync('git', 'add', '-v', './package.json').exit.onerror()
+spawnSync('git', 'commit', '-v', '-m', manifest.version).exit.onerror()
+spawnSync('git', 'tag', `v${manifest.version}`).exit.onerror()
